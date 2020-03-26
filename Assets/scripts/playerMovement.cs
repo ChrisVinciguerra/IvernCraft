40cd5ce011123d821edd9953474b81cb292a8e80
zlassetter <43627906+zlassetter@users.noreply.github.com>
2020-03-26 16:19:47 -0400

    Brought in unity project

---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public bool isGrounded = false;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+        animator.SetFloat("Speed", Input.GetAxis("Horizontal"));
+    }
+    void jump() {
+        if (Input.GetButtonDown("Jump") && isGrounded == true) { 
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 10f), ForceMode2D.Impulse);
+        }
+    }
+}
